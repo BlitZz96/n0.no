ffb6ec436882b74112fa2a34765c517eb2732eab
Sølve Tornøe <Solve@server.local>
2018-05-23 14:47:41 +0200

    Add playSound function that uses html5 & edit toggleMute recordingly

--- src/home.js
+++ src/home.js
@@ -1,4 +1,5 @@
 import lighthouse from './components/lighthouse'
+import riverSound from './assets/water-river.ogg'
 
 export default {
     name: 'home',
@@ -9,12 +10,46 @@ export default {
 
     data() {
         return {
+            audioInstances: {},
+            sounds: {
+                river: riverSound
+            }
         }
     },
 
+    mounted() {
+        this.playSound(this.sounds.river, true)        
+    },
+
     methods: {
+
+        /**
+         * Toggles mute on all instances of audio.
+         */
         toggleMute() {
-            this.$refs.riverSound.muted ^= true;
+            Object.values(this.audioInstances).map(a => a.muted ^= true)
+        },
+
+        /**
+         * Plays a sound via HTMLMediaElement API
+         *  Adds element to object for later manipulating
+         *  - Even your backstreet boys collection is welcome here.
+         * @param {String} src 
+         * @param {Boolean} [loop=false] 
+         */
+        playSound(src, loop = false) {
+            const audioAlreadyExists = Object.keys(this.audioInstances).find(e => e == src);
+
+            if (audioAlreadyExists) {
+                const sound = this.audioInstances[src];                
+                sound.load()
+                sound.play()
+            } else {
+                let sound = new Audio(src);
+                this.audioInstances[src] = sound;
+                sound.loop = loop;
+                sound.play();
+            }
         }
     }
 }
