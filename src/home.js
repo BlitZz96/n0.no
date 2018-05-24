3071fb5fbe43294ace5b474daffc4bcc388bfb8f
BlitZz96 <solve@rocketstudios.no>
2018-05-25 00:16:16 +0200

    Add functionality for clicking an element, playing sound and animate.
    * Add function to register clik eventlistener on svg elements
    * Add anime.js animation framework
    * Add two more sounds
       - For fusebox lever

--- src/home.js
+++ src/home.js
@@ -1,5 +1,8 @@
 import lighthouse from './components/lighthouse'
 import riverSound from './assets/water-river.ogg'
+import leverSound0 from './assets/lever-turn0.ogg'
+import leverSound1 from './assets/lever-turn1.ogg'
+import anime from 'animejs'
 
 export default {
     name: 'home',
@@ -13,15 +16,83 @@ export default {
             audioInstances: {},
             sounds: {
                 river: riverSound
+            },
+            objects: {
+                fuseBox: {
+                    el: null,
+                    sounds: [],
+                    powerLever: {
+                        el: null,
+                        on: false,
+                        sounds: [
+                            leverSound0,
+                            leverSound1
+                        ],
+                    }
+                },
+                toolBox: {
+                    el: null,
+                    sounds: []
+                }
             }
         }
     },
 
     mounted() {
-        this.playSound(this.sounds.river, true)        
+        //Todo: Fix bug regarding play() on start
+        // The user needs to interact with the page before play() can be used.
+        // this.playSound(this.sounds.river, true);
+        this.getElements()
+        this.registerClickListener(this.objects.fuseBox.el, this.RunfuseBox)
     },
 
     methods: {
+
+        registerClickListener(element, method) {
+            element.addEventListener('click', e => {
+                method(element)
+            })
+        },
+
+        /**
+         * Get elements from svg and merge in to objects object.
+         */
+        getElements() {
+            let o = this.objects;
+            o.fuseBox.el = document.getElementById('fuseBox')
+            o.fuseBox.powerLever.el = document.getElementById('powerLever')
+            o.toolBox.el = document.getElementById('toolBox')
+        },
+
+        RunfuseBox(el) {
+            const lever = this.objects.fuseBox.powerLever;
+            let options = {};
+            // transform: rotateX(3deg);
+            // transform - origin: 0px 0 100px;
+
+            //Play different sound & animation relative to lever status
+            if(lever.on) {
+                this.playSound(lever.sounds[0])
+                options = {
+                    rotateX: 0,
+                    duration: 250,
+                }
+            } else {
+                this.playSound(lever.sounds[1])
+                options = {
+                    rotateX: 3,
+                    duration: 400,
+                    delay: 50
+                }
+            }
+
+            //toggle lever & animate
+            lever.on ^= true;
+            anime(Object.assign(options, {
+                targets: lever.el,
+                easing: 'easeInOutQuint'
+            }));
+        },
 
         /**
          * Toggles mute on all instances of audio.
